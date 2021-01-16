26a84e5f81b10349f54b9d8a6c28e6e359f6a2d4
Lucas A <syst.lucasamado@gmail.com>
2021-01-16 15:53:18 -0300

    Start unlimited_react-website

---
+++ unlimited_react-website/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Wassup!</h1>
+      <ul>
+        <li>npm start react-icons --save</li>
+        <li>npm start react-router-dom</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
