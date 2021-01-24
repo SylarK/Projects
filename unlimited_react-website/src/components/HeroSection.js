9b34a827ec45a670c8dfd817c1901fd21a03a037
Lucas A <syst.lucasamado@gmail.com>
2021-01-24 15:40:08 -0300

    Completed.

--- unlimited_react-website/src/components/HeroSection.js
+++ unlimited_react-website/src/components/HeroSection.js
@@ -1,7 +1,7 @@
 import React from 'react';
+import './HeroSection.css';
 import { Button } from './Button';
 import { Link } from 'react-router-dom';
-import './HeroSection.css';
 
 function HeroSection({
   lightBg,
@@ -13,24 +13,24 @@ function HeroSection({
   buttonLabel,
   img,
   alt,
-  imgStart,
+  imgStart
 }) {
   return (
     <>
       <div
         className={lightBg ? 'home__hero-section' : 'home__hero-section darkBg'}
       >
-        <div className="container">
+        <div className='container'>
           <div
-            className="row home__hero-row"
+            className='row home__hero-row'
             style={{
               display: 'flex',
-              flexDirection: imgStart === 'start' ? 'row-reverse' : 'row',
+              flexDirection: imgStart === 'start' ? 'row-reverse' : 'row'
             }}
           >
-            <div className="col">
-              <div className="home__hero-text-wrapper">
-                <div className="top-line">{topLine}</div>
+            <div className='col'>
+              <div className='home__hero-text-wrapper'>
+                <div className='top-line'>{topLine}</div>
                 <h1 className={lightText ? 'heading' : 'heading dark'}>
                   {headline}
                 </h1>
@@ -43,16 +43,16 @@ function HeroSection({
                 >
                   {description}
                 </p>
-                <Link to="/sign-up">
-                  <Button buttonSize="btn--wide" buttonColor="blue">
+                <Link to='/sign-up'>
+                  <Button buttonSize='btn--wide' buttonColor='blue'>
                     {buttonLabel}
                   </Button>
                 </Link>
               </div>
             </div>
-            <div className="col">
-              <div className="home__hero-img-wrapper">
-                <img src="{img}" alt="{alt}" className="home__hero-img" />
+            <div className='col'>
+              <div className='home__hero-img-wrapper'>
+                <img src={img} alt={alt} className='home__hero-img' />
               </div>
             </div>
           </div>
---
+++ unlimited_react-website/src/components/pages/Products/Products.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import HeroSection from '../../HeroSection';
+import { homeObjTwo } from './Data';
+import Pricing from '../../Pricing';
+
+function Products() {
+  return (
+    <>
+      <HeroSection {...homeObjTwo} />
+    </>
+  );
+}
+
+export default Products;
