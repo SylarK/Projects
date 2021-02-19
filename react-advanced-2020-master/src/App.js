014457f43b6acc1bb6adf635781d8a06921a9b09
SylarK <syst.lucasamado@gmail.com>
2021-02-19 08:31:54 -0300

    Initialized app and some config. todo-react-app

--- react-advanced-2020-master/src/App.js
+++ react-advanced-2020-master/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
-//import Setup from './tutorial/3-conditional-rendering/setup/1-multiple-returns';
+import Setup from './tutorial/3-conditional-rendering/setup/2-short-circuit';
 //import Final from './tutorial/1-useState/final/1-error-example'
 //import Training from './training/conditionalRendering/03';
-import Project from './projects/02/todoapp';
+//import Project from './projects/02/todoapp';
 
 function App() {
   return (
     <div className="container">
-      <Project />
+      <Setup />
     </div>
   );
 }
